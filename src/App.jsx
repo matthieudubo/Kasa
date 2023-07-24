1b61e495dcef550d15add236562eb1f0b7cde7e0
Matthieu Dubo <matthieu.dubo40@gmail.com>
2023-07-24 14:13:34 +0200

    removed unused router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./styles/styles.css";
-import { HashRouter, Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
